4d6a47fec7a8d0501347f9150ec22db8f022cd34
yukuto16 <toshima0514@gmail.com>
2022-11-27 02:38:25 +0900

    アラーム機能の実装

--- sample/python/bot_config.py
+++ sample/python/bot_config.py
@@ -3,6 +3,7 @@ NEKO = "/neko"
 HELP = "/help_yukuto"
 HELP_RESULT = "0"
 USER_ID = ""
+TIME = "/time"
 
 REACTION_1 = "Ⅰ"
 REACTION_2 = "Ⅱ"
--- sample/python/discordbot.py
+++ sample/python/discordbot.py
@@ -2,6 +2,8 @@ import pickle
 import discord
 import os
 from sample.python import bot_config
+from discord.ext import tasks
+from datetime import datetime
 
 client = discord.Client()
 
@@ -9,69 +11,43 @@ client = discord.Client()
 # 起動時に動作する処理
 @client.event
 async def on_ready():
-    # dumpファイル作成i
-    PF = [bot_config.HELP_RESUL, bot_config.USER_ID]
-    with open('temp.binaryfile', 'wb') as BF:
-        pickle.dump(PF, BF)
     # 起動したらターミナルにログイン通知が表示される
     print('ログインしました')
 
 
+# タイマー処理
+@tasks.loop(seconds=60)
+async def loop(sec,channel_id):
+    # 現在の時刻
+    NOW = datetime.now().strftime('%H:%M')
+    if NOW == sec:
+        channel = client.get_channel(channel_id)
+        await channel.send('おはよう')  
+        pass
+
+
 # メッセージ受信時に動作する処理
 @client.event
-async def on_message(message):
-    # dumpファイル読み込み
-    with open('temp.binaryfile', 'rb') as BF:
-        PF = pickle.load(BF)
-
+async def on_message(message, time = 0):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
+
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == bot_config.NEKO:
         await message.channel.send('にゃーん')
 
+    # タイマー設定
+    if message.content == bot_config.TIME:
+        CHANNEL_ID = message.channel
+        loop.start(time, CHANNEL_ID)
+
     # /help_yukuto | 要件をどうぞ
     if message.content == bot_config.HELP:
-        HELP_RESULT = 1
-        USER_ID = message.author
-        print(USER_ID)
-        msg = await message.channel.send \
-            ('####要件をどうぞ####\n' \
-             'Ⅰ：アラームの設定\n' \
-             # TODO
-             # Ⅱ：自動切断設定\n' \
-             'Ⅲ：なんでもない')
-        await msg.add_reaction(bot_config.REACTION_1)
-        # TODO
-        # await msg.add_reaction("bot_config.REACTION_2")
-        await msg.add_reaction(bot_config.REACTION_3)
-
-        msg_id = msg.id
-
-    # /help_yukuto -> Ⅰ
-    # ネスト構造
-    # TODO
-    # リアクションされた時点で呼び出される？
-    # トリガーとなるリアクションを設定できない？
-    async def on_raw_reaction_add(res):
-        if res.message_id == msg_id:
-
-            # TODO
-            # アラーム処理の関数を呼び出す
-
-
-        # dumpファイル保存
-        PF = [HELP_RESULT, USER_ID]
-        with open('temp.binaryfile', 'wb') as BF:
-            pickle.dump(PF, BF)
-
-
-# await message.channel.send \
-# ('####アラームを設定しますか？####')
-
-# 書き出し処理
-joblib.dump(bot_config)
+        await message.channel.send \
+            ('####コマンド一覧####\n' \
+             '/time xx:xx | アラームの設定\n' \
+             '/neko | 鳴き声\n')
 
 # Botの起動とDiscordサーバーへの接続
 client.run(os.environ["TOKEN"])
